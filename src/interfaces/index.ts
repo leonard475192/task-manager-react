255533750430bc0ec55caf1f34fae024dfa5e2b5
leonard475192 <leonard.t1028@gmail.com>
2022-06-06 18:08:57 +0900

    :bug: fix: api レスポンス undefinedの考慮

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,18 +8,18 @@ export type TaskStatus = keyof typeof taskStatuses
 export type TaskReq = {
   title: string
   status: TaskStatus
-  content: string
-  manHour: number
-  deadlineAt: Date
+  content: string | undefined  // TODO string?でできなかった
+  manHour: number | undefined
+  deadlineAt: Date | undefined
 }
 
 export type TaskRes = {
   id: number
   title: string
   status: TaskStatus
-  content: string
-  manHour: number
-  deadlineAt: Date
+  content: string | undefined
+  manHour: number | undefined
+  deadlineAt: Date | undefined
   createdAt: Date
   updateAt: Date
 }
